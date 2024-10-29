3f6c0256f432dd3a08ee1b33d9b674b1426ef462
Fafnir Nokkvason <wulfiesocial@gmail.com>
2024-10-29 20:51:31 +0100

    Created page for creation, set up controller for basic functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,1 +1,11 @@
-"#controller.py" 
+from main import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    return render_template('create.html')
