e68731a8f54c2b8d7cf3b35ea857fd8d7a0c5a2c
Fafnir Nokkvason <wulfiesocial@gmail.com>
2024-10-30 14:46:57 +0100

    Created more HTML files and Flask endpoints, manual database update on front-end is mostly done.

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,9 +6,42 @@ from flask import render_template, request
 def index():
     return render_template('index.html')
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/emplog/', methods=['GET', 'POST'])
+def emplog(logged_in=False):
+    if request.method == 'POST':
+        logged_in = True
+
+    return render_template('employeelogin.html', emp=logged_in)
+
+
+################
+# CRUD routing #
+################
+
+@app.route('/create/', methods=['GET', 'POST'])
 def create(name=None):
     if request.method == 'POST':
         name = request.form['name']
 
     return render_template('create.html', name = name)
+
+@app.route('/read/', methods=['GET', 'POST'])
+def read(name=None):
+    if request.method == 'POST':
+        name = request.form['name']
+
+    return render_template('read.html', name = name)
+
+@app.route('/update/', methods=['GET', 'POST'])
+def update(name=None):
+    if request.method == 'POST':
+        name = request.form['name']
+
+    return render_template('update.html', name = name)
+
+@app.route('/delete/', methods=['GET', 'POST'])
+def delete(name=None):
+    if request.method == 'POST':
+        name = request.form['name']
+
+    return render_template('delete.html', name = name)
