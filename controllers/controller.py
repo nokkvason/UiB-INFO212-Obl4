925a479a27bb52b0aca1e11e619edd783dbd2771
Fafnir Nokkvason <wulfiesocial@gmail.com>
2024-10-30 15:26:03 +0100

    Ok, let's try this again.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,14 +1,14 @@
 from main import app
-from flask import render_template, request
+from flask import request
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return
 
 @app.route('/create', methods=['GET', 'POST'])
 def create(name=None):
     if request.method == 'POST':
         name = request.form['name']
 
-    return render_template('create.html', name = name)
+    return
