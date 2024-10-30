bf9b4db662bea14bb77585be1175e7b72045c4e4
Fafnir Nokkvason <wulfiesocial@gmail.com>
2024-10-30 15:39:46 +0100

    Some postman testing, think I've got it now.

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,9 +2,17 @@ from main import app
 from flask import request
 
 
-@app.route('/')
-def index():
-    return
+@app.route('/', methods=['GET', 'POST'])
+def index(data=None):
+    if request.method == 'POST':
+        data = request.args['sent_data']
+
+        if data is not None:
+            return 'POST with param seems to work'
+        
+        return 'POST(no param) seems to work'
+
+    return 'GET seems to work'
 
 @app.route('/create', methods=['GET', 'POST'])
 def create(name=None):
